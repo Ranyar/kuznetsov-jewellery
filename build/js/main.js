2f571ed86e86998f08bff233a005942b33bf40fa
Ranyar <r4n74r@gmail.com>
2022-03-18 02:05:41 +0500

    Добавляет разметку и логику работы аккордеона на JS

--- build/js/main.js
+++ build/js/main.js
@@ -1,53 +1,64 @@
-'use strict';
+`use strict`;
 
-const pageBody = document.querySelector('.page-body');
-const pageHeaderWrapper = document.querySelector('.page-header__wrapper');
-const mainNav = document.querySelector('.main-nav');
-const mainNavListPrimary = document.querySelector('.main-nav__list--primary');
-const toggleButton = document.querySelector('.page-header__menu-toggle');
-const cartIcon = document.querySelector('.page-header__cart');
-const logoIcon = document.querySelector('.page-header__logo');
-const userMenu = document.querySelector('.user-menu');
-const search = document.querySelector('.page-header__search');
+// ----- Меню
 
-// mainNav.classList.remove('main-nav--nojs');
+const pageBody = document.querySelector(`.page-body`);
+const pageHeaderWrapper = document.querySelector(`.page-header__wrapper`);
+const mainNav = document.querySelector(`.main-nav`);
+const mainNavListPrimary = document.querySelector(`.main-nav__list--primary`);
+const toggleButton = document.querySelector(`.page-header__menu-toggle`);
+const cartIcon = document.querySelector(`.page-header__cart`);
+const logoIcon = document.querySelector(`.page-header__logo`);
+const userMenu = document.querySelector(`.user-menu`);
+const search = document.querySelector(`.page-header__search`);
 
-toggleButton.addEventListener('click', function () {
-  if (pageHeaderWrapper.classList.contains('page-header__wrapper--toggled')) {
-    pageBody.classList.remove('page-body--lock');
-    pageHeaderWrapper.classList.remove('page-header__wrapper--toggled');
-    pageHeaderWrapper.classList.remove('page-header__wrapper--lock');
-    mainNav.classList.remove('main-nav--toggled');
-    mainNav.classList.remove('main-nav__list--toggled')
-    toggleButton.classList.remove('page-header__menu-toggle--toggled');
-    cartIcon.classList.remove('page-header__cart--toggled');
-    logoIcon.classList.remove('page-header__logo--toggled');
-    userMenu.classList.remove('user-menu--toggled');
-    search.classList.remove('page-header__search--toggled');
+toggleButton.addEventListener(`click`, function () {
+  if (pageHeaderWrapper.classList.contains(`page-header__wrapper--toggled`)) {
+    pageBody.classList.remove(`page-body--lock`);
+    pageHeaderWrapper.classList.remove(`page-header__wrapper--toggled`);
+    pageHeaderWrapper.classList.remove(`page-header__wrapper--lock`);
+    mainNav.classList.remove(`main-nav--toggled`);
+    mainNav.classList.remove(`main-nav__list--toggled`);
+    toggleButton.classList.remove(`page-header__menu-toggle--toggled`);
+    cartIcon.classList.remove(`page-header__cart--toggled`);
+    logoIcon.classList.remove(`page-header__logo--toggled`);
+    userMenu.classList.remove(`user-menu--toggled`);
+    search.classList.remove(`page-header__search--toggled`);
   } else {
-    pageBody.classList.add('page-body--lock');
-    pageHeaderWrapper.classList.add('page-header__wrapper--toggled');
-    pageHeaderWrapper.classList.add('page-header__wrapper--lock');
-    mainNav.classList.add('main-nav--toggled');
-    mainNav.classList.add('main-nav__list--toggled')
-    toggleButton.classList.add('page-header__menu-toggle--toggled');
-    cartIcon.classList.add('page-header__cart--toggled');
-    logoIcon.classList.add('page-header__logo--toggled');
-    userMenu.classList.add('user-menu--toggled');
-    search.classList.add('page-header__search--toggled');
+    pageBody.classList.add(`page-body--lock`);
+    pageHeaderWrapper.classList.add(`page-header__wrapper--toggled`);
+    pageHeaderWrapper.classList.add(`page-header__wrapper--lock`);
+    mainNav.classList.add(`main-nav--toggled`);
+    mainNav.classList.add(`main-nav__list--toggled`);
+    toggleButton.classList.add(`page-header__menu-toggle--toggled`);
+    cartIcon.classList.add(`page-header__cart--toggled`);
+    logoIcon.classList.add(`page-header__logo--toggled`);
+    userMenu.classList.add(`user-menu--toggled`);
+    search.classList.add(`page-header__search--toggled`);
   }
 });
 
-// for (let i = 0; i < mainNavItems.length; i++) {
-//   mainNavItems[i].addEventListener('click', function () {
-//     if (mainNav.classList.contains('main-nav--opened')) {
-//       pageBody.classList.remove('page-body--lock');
-//       mainNav.classList.remove('main-nav--opened');
-//       toggleButton.classList.add('main-nav__toggle--closed');
-//     }
-//   });
-// }
+// ----- Аккордеон
 
-// показать меню - жс
-// показать кусок юзер-меню - жс
-// перекрасить пункты меню - жс
+const accordions = document.querySelectorAll(`.accordion`);
+
+if (accordions) {
+  const headers = document.querySelectorAll(`.accordion h3`);
+  const content = document.querySelectorAll(`.accordion__content`);
+
+  for (let i = 0; i < headers.length; i++) {
+    headers[i].addEventListener(`click`, () => {
+      let shown = document.querySelector(`.accordion__content--show`);
+
+      if (shown === headers[i]) {
+        content[i].classList.remove(`accordion__content--show`);
+      } else {
+        content[i].classList.add(`accordion__content--show`);
+      }
+
+      if (shown) {
+        shown.classList.remove(`accordion__content--show`);
+      }
+    });
+  }
+}
